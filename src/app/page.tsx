3651f5280cbf31e90433edf08f00aaaecbf3c92c
Rai Ammad Khan <ammad.khan@northbaysolutions.net>
2025-04-13 19:19:19 +0500

    removed unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import { redirect } from 'next/navigation';
-import Image from "next/image";
-import Link from 'next/link';
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect('/dashboard');
+  redirect("/dashboard");
 }
